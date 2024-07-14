3f8a8747f2a707996ff55bd3d823026fe40d5264
Sumedh Mhaske <sumedhmhaske310503@gmail.com>
2024-07-14 11:55:50 +0530

    Added another class Cycling extending Workout clas

--- script.js
+++ script.js
@@ -41,3 +41,19 @@ class Running extends Workout {
     return this.pace;
   }
 }
+
+class Cycling extends Workout {
+  type = "cycling";
+
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/hr
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
